256265e00cc88913ab0813fada49f0f224020cdd
ermessias <ermessias@uolinc.com>
2023-08-26 17:37:43 -0300

    Iniciando projeto para estudo dos padroes de projetos comportamentais

---
+++ src/br/com/messias/loja/orcamento/Orcamento.java
@@ -0,0 +1,25 @@
+package br.com.messias.loja.orcamento;
+
+import java.math.BigDecimal;
+
+public class Orcamento {
+
+	private BigDecimal valor;
+	
+
+	public Orcamento(BigDecimal valor) {
+		super();
+		this.valor = valor;
+	}
+
+	public BigDecimal getValor() {
+		return valor;
+	}
+
+	public void setValor(BigDecimal valor) {
+		this.valor = valor;
+	}
+
+	
+	
+}
