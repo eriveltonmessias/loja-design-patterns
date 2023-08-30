39aa723e36ab4717f6c246505da5f14696a659e7
ermessias <ermessias@uolinc.com>
2023-08-30 20:53:45 -0300

    aplicando a regra para o desconto extra

--- src/br/com/messias/loja/orcamento/Orcamento.java
+++ src/br/com/messias/loja/orcamento/Orcamento.java
@@ -6,12 +6,27 @@ public class Orcamento {
 
 	private BigDecimal valor;
 	private int qtdItens;
+	private String situacao;
 	
 
 	public Orcamento(BigDecimal valor, int qtdItens) {
 		super();
 		this.valor = valor;
 		this.qtdItens = qtdItens;
+	}
+	
+	
+	public void aplicarDescontoExtra() {
+		
+		BigDecimal valorDoDescontoExtra = BigDecimal.ZERO;
+		
+		if(situacao.equals("EM_ANALISE")) {
+			valorDoDescontoExtra = new BigDecimal("0.05");
+		}else if(situacao.equals("APROVADO")) {
+			valorDoDescontoExtra = new BigDecimal("0.02");
+		}
+		
+		this.valor = this.valor.multiply(valorDoDescontoExtra);
 	}
 
 	public BigDecimal getValor() {
