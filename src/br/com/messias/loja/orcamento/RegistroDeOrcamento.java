56d4f72733928b1988f15d738804255d55f6a292
ermessias <ermessias@uolinc.com>
2023-09-05 17:30:05 -0300

    Criando classe que irá fazer a chama via API para registrar o orcamento

---
+++ src/br/com/messias/loja/orcamento/RegistroDeOrcamento.java
@@ -0,0 +1,12 @@
+package br.com.messias.loja.orcamento;
+
+public class RegistroDeOrcamento {
+	
+	public void registrar(Orcamento orcamento) {
+		
+		// chamada HTTP para API externa
+		// URL connection
+		// http client
+	}
+
+}
