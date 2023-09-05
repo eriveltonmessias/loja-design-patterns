65ea9410c12748068a62c63c179c6c9c29f34e33
ermessias <ermessias@uolinc.com>
2023-09-05 08:59:00 -0300

    Criando as classe para salvar o pedido no BD e enviar o email

--- src/br/com/messias/loja/pedido/GeraPedidoHandler.java
+++ src/br/com/messias/loja/pedido/GeraPedidoHandler.java
@@ -3,6 +3,8 @@ package br.com.messias.loja.pedido;
 import java.time.LocalDateTime;
 
 import br.com.messias.loja.orcamento.Orcamento;
+import br.com.messias.loja.pedido.acao.EnviarEmailPedido;
+import br.com.messias.loja.pedido.acao.SalvarPedidoNoBancoDeDados;
 
 public class GeraPedidoHandler /*implements commandsInterface*/{
 	
@@ -11,9 +13,12 @@ public class GeraPedidoHandler /*implements commandsInterface*/{
 	public void execute(GerarPedido dados) {
 		Orcamento orcamento = new Orcamento(dados.getValorOrcamento(), dados.getQtdItens());
 		Pedido pedido = new Pedido(dados.getCliente(), LocalDateTime.now(), orcamento);
-		
-		System.out.println("Salvar pedido na base");
-		System.out.println("Enviar email com dados do novo pedido");
+
+		EnviarEmailPedido emailPedido = new EnviarEmailPedido();
+		emailPedido.executar(pedido);
+		SalvarPedidoNoBancoDeDados bancoDeDados = new SalvarPedidoNoBancoDeDados();
+		bancoDeDados.executar(pedido);
+
 	}
 
 }
---
+++ src/br/com/messias/loja/pedido/acao/EnviarEmailPedido.java
@@ -0,0 +1,11 @@
+package br.com.messias.loja.pedido.acao;
+
+import br.com.messias.loja.pedido.Pedido;
+
+public class EnviarEmailPedido {
+	
+	public void executar(Pedido pedido) {
+		System.out.println("Enviar email com dados do pedido");
+	}
+
+}
